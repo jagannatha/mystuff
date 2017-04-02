4d4065e8a0316955742a8c3cfe8c0de84a56202b
Jagannath <jagannatham66@gmail.com>
2017-04-02 18:39:38 +0530

    Python execises

---
+++ Basic_Examples/if.py
@@ -0,0 +1,10 @@
+x=int (raw_input("Please enter an integer:"))
+if x<0:
+    x=0
+print 'Navigate changed to zero'
+elif:
+    print 'Zero'
+elif x==1:
+print 'Single'
+else:
+print 'More'
---
+++ learnpythonthehardway/ex30.py
@@ -0,0 +1,19 @@
+people=30
+cars=40
+buses=15
+if cars>people:
+ print"We should take the  cars"
+elif cars<people:
+ print"we should not take the cars"
+else:
+  print "We can't decide"
+  if buses>cars:
+        print"That's too many buses"
+  elif buses<cars:
+        print "Maybe we could take the buses."
+  else:
+        print"We still cars are fast"
+  if people >buses:
+        print "alright, let's go"
+  else:
+    print "Fine, let;s go buses."
